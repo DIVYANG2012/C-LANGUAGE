4892e05de152fa199a84dae71a03ad75caf213b2
DIVYANG2012 <134709151+DIVYANG2012@users.noreply.github.com>
2023-05-29 18:52:38 +0530

    Add files via upload

---
+++ day2/q2divyang.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+
+int main()
+{
+    printf("*");
+    printf("\n*");
+    printf("\n*                * *");
+    printf("\n*               *   *");
+    printf("\n*    * *       *");
+    printf("\n*   *   *     *");
+    printf("\n*  *     *   *");
+    printf("\n* *       * *");
+    printf("\n*");
+    return 0;
+}
+
---
+++ day2/q3(divyang).c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf(" _ _ _ ");
+    printf("\n|     |");
+    printf("\nN     |");
+    printf("\nA     |");
+    printf("\nM     |");
+    printf("\nE     |");
+    printf("\n|     |");
+    printf("\n _ _ _");
+    return 0;
+}
